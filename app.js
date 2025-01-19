ca544b3fcc356e779b08ed1cda12ddbf40fa1186
rakibulhasan933 <rakibulmd933@gmail.com>
2025-01-19 10:30:53 +0600

    [UPDATE] file

--- app.js
+++ app.js
@@ -6,20 +6,21 @@ async function main() {
         spinner.style.display = "block";
         const res = await fetch("https://www.rakibulhasanrakib.com/api/foods");
         const foods = await res.json();
+        // console.log(foods);
 
-
-        foods.forEach((food) => {
+        foods.forEach((pd) => {
+            // console.log(food);
             const box = document.createElement("div");
             box.style.border = "2px solid green";
             box.style.padding = "10px";
             box.style.borderRadius = "8px";
 
             box.innerHTML = `
-        <h1 class="name">${food.name} <i class="fa fa-cart-plus" aria-hidden="true"></i></h1> 
-        <h4>Category: ${food.category} </h4> 
-        <h3 class="price">Price: $${food.price} </h3> 
-        <p>${food.isVegetarian ? "Vegetarian" : "Non-Vegetarian"}</p> 
-        <img src="${food.image}" width="500" height="300" alt="${food.name}"> 
+        <h1 class="name">${pd.name} <i class="fa fa-cart-plus" aria-hidden="true"></i></h1> 
+        <h4>Category: ${pd.category} </h4> 
+        <h3 class="price">Price: &#2547; ${pd.price} </h3> 
+        <p>${pd.isVegetarian ? "Vegetarian" : "Non-Vegetarian"}</p> 
+        <img src="${pd.image}" width="500" height="300" alt="${pd.name}"> 
         `;
 
             boxContainer.append(box);
